1609fafaa3460106b008b3e029bd8ae5c397e603
sl4shbored <39769731+sl4shbored@users.noreply.github.com>
2019-05-31 17:23:56 +0200

    changed listen

--- webserver.js
+++ webserver.js
@@ -5,5 +5,5 @@ http.createServer(function ( request, response){
     var urlString = url.parse(request.url, true);
     var body = 'Hello ' + urlString.query.name;
     response.end(body);
-}).listen(3000, '0.0.0.0');
+}).listen(3000);
 console.log('running webserver');
